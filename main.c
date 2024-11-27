1bf4e4b2935fceaa2fe1f6e2a1db9ec30798771d
Kcits970 <geometricstick@gmail.com>
2024-11-27 21:47:41 +0900

    created a ludicrously basic outline in main.c

---
+++ main.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void boot()
+{
+	// allocate system resources (conceptually).
+}
+
+void poweroff()
+{
+	// clear all system resources (conceptually).
+}
+
+int main()
+{
+	boot();
+	poweroff();
+
+	return 0;
+}
