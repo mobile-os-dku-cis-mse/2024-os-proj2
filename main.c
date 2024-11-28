ffbb36bac738582721ca8455311fb30cadc848a4
Kcits970 <geometricstick@gmail.com>
2024-11-29 01:16:00 +0900

    added signal handling system

--- main.c
+++ main.c
@@ -3,34 +3,49 @@
 #include <unistd.h>
 #include <signal.h>
 #include <sys/types.h>
+#include <sys/time.h>
 
 #include "ds/pidq.h"
+#include "wrap/wrap.h"
 
 pidq rqueue;
+int ticks;
 
 void run()
 {
 	while (1);
 }
 
-void schedule()
+void alarm_handler(int)
 {
-	return;
+	ticks++;
 }
 
-void poweroff()
+void enable_ticks()
+{
+	struct itimerval itimerval = {{0, 10000}, {0, 10000}};
+	setitimer(ITIMER_REAL, &itimerval, NULL);
+}
+
+void disable_ticks()
+{
+	struct itimerval itimerval = {{0, 0}, {0, 0}};
+	setitimer(ITIMER_REAL, &itimerval, NULL);
+}
+
+void cleanup()
 {
 	while (!pidq_empty(&rqueue))
 		kill(pidq_pop(&rqueue), SIGTERM);
 
 	pidq_destroy(&rqueue);
-
 	exit(0);
 }
 
-void boot()
+void spawn()
 {
 	pidq_init(&rqueue, 10);
+
 	while (!pidq_full(&rqueue))
 	{
 		pid_t pid = fork();
@@ -38,7 +53,7 @@ void boot()
 		if (pid == -1)
 		{
 			perror("fork");
-			poweroff();
+			cleanup();
 		}
 		
 		if (pid)
@@ -46,14 +61,32 @@ void boot()
 		else
 			run();
 	}
+}
 
-	schedule();
+void schedule()
+{
+	printf("scheduler invoked: %d\n", ticks);
+}
+
+void loop()
+{
+	my_sigaction(SIGALRM, alarm_handler);
+	enable_ticks();
+
+	while (ticks < 100)
+	{
+		pause();
+		schedule();
+	}
+
+	disable_ticks();
 }
 
 int main()
 {
-	boot();
-	poweroff();
+	spawn();
+	loop();
+	cleanup();
 
 	return 0;
 }
