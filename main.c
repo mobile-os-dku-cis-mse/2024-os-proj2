358dfbbb197adbb4526da1e25b44baa1d94f1c7b
Kcits970 <geometricstick@gmail.com>
2024-12-03 16:07:53 +0900

    implemented log messages

--- main.c
+++ main.c
@@ -36,10 +36,8 @@ void mem_init()
 	mem = calloc(MEM_SIZE, sizeof(unsigned int));
 	page_list = calloc(PAGE_COUNT, sizeof(unsigned int));
 
-	for (int i = 1; i < PAGE_COUNT; i++)
-		page_list[i] = page_list[i-1] + PAGE_SIZE;
-
-	page_ptr = 0;
+	for (int i = 0; i < PAGE_COUNT; i++)
+		page_list[i] = i;
 }
 
 unsigned int mem_translate(unsigned int vaddr)
@@ -59,37 +57,36 @@ unsigned int mem_translate(unsigned int vaddr)
 	{
 		cur.page_tbl[off1] = malloc(ENTRY_COUNT * sizeof(unsigned int));
 		memset(cur.page_tbl[off1], 0xFF, ENTRY_COUNT * sizeof(unsigned int));
+		dprintf(logfd, "\t\t-> new page table allocated\n");
 	}
 
 	if (cur.page_tbl[off1][off2] == -1)
 	{
 		if (page_ptr == PAGE_SIZE)
+		{
+			dprintf(logfd, "\t\t-> cannot assign new page frame; not enough memory\n");
 			return -1;
+		}
 
 		cur.page_tbl[off1][off2] = page_list[page_ptr++];
+		dprintf(logfd, "\t\t-> new page frame assigned\n");
 	}
 
-	return cur.page_tbl[off1][off2] + off;
+	unsigned int addr = (cur.page_tbl[off1][off2] << 8) + off;
+	dprintf(logfd, "\t\t-> translated to physical address 0x%05X\n", addr);
+	return addr;
 }
 
-int mem_read(unsigned int vaddr)
+int mem_read(unsigned int addr)
 {
-	unsigned int target = mem_translate(vaddr);
-
-	if (target == -1);
-		//printf("error");
-	else
-		return mem[target];
+	dprintf(logfd, "\t\t-> [0x%05X] contains 0x%08X\n", addr, mem[addr]);
+	return mem[addr];
 }
 
-void mem_write(unsigned int vaddr, unsigned int val)
+void mem_write(unsigned int addr, unsigned int val)
 {
-	unsigned int target = mem_translate(vaddr);
-
-	if (target == -1);
-		//printf("error");
-	else
-		mem[target] = val;
+	mem[addr] = val;
+	dprintf(logfd, "\t\t-> 0x%08X written to [0x%05X]\n", val, addr);
 }
 
 void alarm_handler(int)
@@ -155,6 +152,8 @@ void schedule()
 {
 	static int remaining = TIME_QUANTUM;
 
+	dprintf(logfd, "log message at tick %d\n", ticks);
+	dprintf(logfd, "process[%d] gets cpu time\n", cur.pid);
 	kill(cur.pid, SIGCONT);
 
 	struct msgbuf buf;
@@ -162,13 +161,19 @@ void schedule()
 
 	for (int i = 0; i < 10; i++)
 	{
+		dprintf(logfd, "\tvirtual address 0x%05X\n", buf.vaddr[i]);
+		unsigned int addr = mem_translate(buf.vaddr[i]);
+
+		if (addr == -1)
+			continue;
+
 		// write flag.
 		if (buf.rwflag & (1 << i))
-			mem_write(buf.vaddr[i], buf.rwval[i]);
+			mem_write(addr, buf.rwval[i]);
 
 		// read flag.
 		else
-			buf.rwval[i] = mem_read(buf.vaddr[i]);
+			mem_read(addr);
 	}
 
 	if (!--remaining)
@@ -176,7 +181,10 @@ void schedule()
 		pcbq_push(&rqueue, cur);
 		cur = pcbq_pop(&rqueue);
 		remaining = TIME_QUANTUM;
+		dprintf(logfd, "context switched\n");
 	}
+
+	dprintf(logfd, "\n\n");
 }
 
 void loop()
@@ -187,7 +195,7 @@ void loop()
 	set_sa_handler(SIGALRM, alarm_handler);
 	enable_ticks();
 
-	while (ticks < 100)
+	while (ticks < 1000)
 	{
 		pause();
 		schedule();
